44b3294ac6bea7c9b5debc8b0ba415dc68f7c7b8
Srinivas <srinivas.bathuru@gmail.com>
2019-02-24 12:07:58 +0530

    update

--- src/main/java/com/devops/springboot/controlller/EmployeeController.java
+++ src/main/java/com/devops/springboot/controlller/EmployeeController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.devops.springboot.model.Employee;
@@ -18,31 +22,31 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 
-	@RequestMapping(value="/employees", method = RequestMethod.GET)
+	@GetMapping(value="/employees")
 	public List<Employee> getAllEmployees() {
 
 		return employeeService.getAllEmployees();
 	}
 
-	@RequestMapping(value="/employees/{id}", method = RequestMethod.GET)
+	@GetMapping(value="/employees/{id}")
 	public Employee getEmployee(@PathVariable int id) {
 
 		return employeeService.getEmployee(id);
 	}
 
-	@RequestMapping(value = "/employees", method = RequestMethod.POST)
+	@PostMapping(value = "/employees")
 	public void addEmployee(@RequestBody Employee employee) {
 
 		employeeService.addEmployee(employee);
 	}
 
-	@RequestMapping(value = "/employees/{id}", method = RequestMethod.PUT)
+	@PutMapping(value = "/employees/{id}")
 	public void updateEmployee(@RequestBody Employee employee, @PathVariable int id) {
 
 		employeeService.updateEmployee(id, employee);
 	}
 
-	@RequestMapping(value = "/employees/{id}", method = RequestMethod.DELETE)
+	@DeleteMapping(value = "/employees/{id}")
 	public void deleteEmployee(@PathVariable int id) {
 
 		employeeService.deleteEmployee(id);
